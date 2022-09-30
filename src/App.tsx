cce8820781854cab3a41fda4da124095df4c0cad
enckao <nicolasthiriet@outlook.fr>
2022-09-30 16:24:26 +0200

    💄 ✨ 🌐 ajout trad FR/EN et switch theme dark/light

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,51 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import Home from './components/Home/home';
-import {
-  createTheme,
-  CssBaseline,
-  ThemeProvider,
-  useMediaQuery,
-} from '@mui/material';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import './utils/i18n/config';
 import { PrivateRouter } from './router/privateRouter';
+import AppDrawer from './components/Menu/appDrawer';
+
+export const ColorModeContext = React.createContext({
+  toggleColorMode: () => {},
+});
 
 const App: React.FC = () => {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
 
   const theme = React.useMemo(
     () =>
       createTheme({
         palette: {
-          mode: prefersDarkMode ? 'dark' : 'light',
+          mode,
         },
       }),
-    [prefersDarkMode],
+    [mode],
   );
   return (
     <RecoilRoot>
       <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/*" element={<PrivateRouter />} />
-          </Routes>
-        </ThemeProvider>
+        <ColorModeContext.Provider value={colorMode}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <AppDrawer
+              inBoxContent={
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/*" element={<PrivateRouter />} />
+                </Routes>
+              }
+            />
+          </ThemeProvider>
+        </ColorModeContext.Provider>
       </BrowserRouter>
     </RecoilRoot>
   );
--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -1,57 +1,33 @@
 import React from 'react';
-import {
-  Box,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  Typography,
-  useTheme,
-} from '@mui/material';
+import { Button, Grid, Typography } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { useTranslation } from 'react-i18next';
 
 const Home: React.FC = () => {
-  const theme = useTheme();
+  const { t } = useTranslation();
   return (
-    <Box
-      sx={{
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        bgcolor: theme.palette.grey['300'],
-        color: 'text.primary',
-      }}
-    >
-      <Card raised sx={{ bgcolor: theme.palette.primary.light }}>
-        <CardContent>
-          <Typography variant="h5" component="div">
-            WannaPlay ?
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            tired of texting friends ?
-          </Typography>
-          <Typography variant="body2">
-            Just register with your Discord account, fill your profile,
-            subscribe to your friend's status feed and don't worry anymore.
-          </Typography>
-        </CardContent>
-        <CardActions sx={{ justifyContent: 'center' }}>
-          <Button
-            size="small"
-            variant="contained"
-            endIcon={<FontAwesomeIcon icon={faDiscord} />}
-            onClick={() => {
-              window.location.href = 'http://localhost:3333/api/auth/login';
-            }}
-          >
-            Connect with your Discord account
-          </Button>
-        </CardActions>
-      </Card>
-    </Box>
+    <Grid container spacing={2} sx={{ textAlign: 'center', mt: '5em' }}>
+      <Grid item xs={12}>
+        <Typography variant="h1">WannaPlay ?</Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          {t('home.subtitle')}
+        </Typography>
+        <Typography variant="body2">{t('home.description')}</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Button
+          size="small"
+          variant="contained"
+          endIcon={<FontAwesomeIcon icon={faDiscord} />}
+          onClick={() => {
+            window.location.href = 'http://localhost:3333/api/auth/login';
+          }}
+        >
+          {t('home.connection_button')}
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
